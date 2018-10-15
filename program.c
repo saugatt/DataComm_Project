a4679c4d9720663e3100de53f23f8fcda2287980
Saugat Tripathi <saugat.tripathi@bison.howard.edu>
2018-10-14 23:54:10 -0400

    readfile  added

--- program.c
+++ program.c
@@ -4,6 +4,25 @@
 #include <stdlib.h>
 
 
+int readFile(char* filename) {
+
+	// byte count
+    int c = 0;
+    FILE *pointer;
+    pointer = fopen("practice_project_test_file_1","rb");
+    int my_arr[500];
+    unsigned char buffer[1];
+ 
+    // save num to array
+    while (fread(&buff,sizeof(buff),1,pointer) != 0) {
+        my_arr[c] = buff[0];
+        c += 1;
+    }
+    return c;
+
+}
+
+
 void writeFirstType(FILE* filename, unsigned char type, int length, int numbers[]) {
 
 	fprintf(filename, "%c", type+1);
@@ -27,13 +46,12 @@ void writeFirstType(FILE* filename, unsigned char type, int length, int numbers[
 void writeSecondType(FILE* filename, unsigned char type, int length, int numbers[]) {	
 	// writing type character
 	fprintf(filename, "%c", type-1);
-
 	// fwrite writes integer byte
 	fwrite(&length, 1, 1, filename);
 	// flipping the length to match the order
 	int flip_num[length];
 
-	for (int i=0; i<length; i++) {
+	for (int i = 0; i < length; i++) {
 		flip_num[i] = (numbers[i]>>8) | (numbers[i]<<8);
 		fwrite(&flip_num[i] , 2, 1, filename);
 	}
