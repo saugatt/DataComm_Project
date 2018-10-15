e903ccdd94fcdc9290eca3d78b24d8f2a7c251f1
Saugat Tripathi <saugat.tripathi@bison.howard.edu>
2018-10-14 23:32:57 -0400

    writeSecondType added

--- program.c
+++ program.c
@@ -24,3 +24,19 @@ void writeFirstType(FILE* filename, unsigned char type, int length, int numbers[
 	
 }
 
+void writeSecondType(FILE* filename, unsigned char type, int length, int numbers[]) {	
+	// writing type character
+	fprintf(filename, "%c", type-1);
+
+	// fwrite writes integer byte
+	fwrite(&length, 1, 1, filename);
+	// flipping the length to match the order
+	int flip_num[length];
+
+	for (int i=0; i<length; i++) {
+		flip_num[i] = (numbers[i]>>8) | (numbers[i]<<8);
+		fwrite(&flip_num[i] , 2, 1, filename);
+	}
+}
+
+
