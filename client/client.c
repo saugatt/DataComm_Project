6a018e796fde186d85002c478fcb070a31fcbf56
Saugat Tripathi <saugat.tripathi@bison.howard.edu>
2018-10-18 22:08:42 -0400

    Final bug fixes for the client

--- client/client.c
+++ client/client.c
@@ -1,9 +1,7 @@
 /*
 Saugat Tripathi
 @02752994
-
 */
-
 
 #include <sys/types.h>      /*  socket types           */
 #include <unistd.h>         /*  misc. UNIX functions   */
@@ -25,13 +23,8 @@ Saugat Tripathi
 int ParseCmdLine(int argc, char *argv[], char **sAddr, char **sPort, char **readFile, char** type, char** saveFile);
 void connectionHandler(int conn_s, char* type, char* readFile, char* saveFile);
 
-
-int main(int argc, char *argv[]) {
-
-    char     *type;                 // type variable                    
-    char     *readFile;             //  file to read by client           
-    char     *saveFile;             //  file name to be saved at server  
-    int      state;                 //   state of process  
+int main(int argc, char *argv[]) 
+{
 
     int       conn_s;                //  connection socket         
     short int port;                  // port number               
@@ -40,37 +33,36 @@ int main(int argc, char *argv[]) {
     char     *sAddr;                 // remote IP address   
     char      buff[LINE_MAXIMUM];  // character buff       
     char     *sPort;                 //remote port 
-   
 
+    char     *type;                  
+    char     *readFile;             
+    char     *saveFile;           
+    int      state;                
 
-
-    /*  Get command line arguments  */
-
+    //  Get command line arguments  
     ParseCmdLine(argc, argv, &sAddr, &sPort, &readFile, &type, &saveFile);
 
 
     // argument validation and setup into appropriate struct begins here
 
     // strstr checkes if type has 0, 1, 2 or 3
-
-   if (strstr("0123", type) == NULL) 
+    if (strstr("0123", type) == NULL) 
     {
         perror("Only type 0, 1, 2 or 3 is supported by the server");
-        exit(1);
     }
 
     // validating the gathered port and setting it to the port variable
-     port = strtol(sPort, &endptr, 0);
+    port = strtol(sPort, &endptr, 0);
     if ( *endptr ) 
     {
-        printf("ECHOCLNT: The port supplited is not valide\n");
+        printf("The port supplited is not valide\n");
         exit(EXIT_FAILURE);
     }
-
-	// creating listening socket
-    if ((conn_s = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) 
+    
+    // creating listening socket
+    if ( (conn_s = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) 
     {
-        fprintf(stderr, "ECHOCLNT: Could not create a listening socket.\n");
+        fprintf(stderr, "Could not create a listening socket.\n");
         exit(EXIT_FAILURE);
     }
 
@@ -80,73 +72,70 @@ int main(int argc, char *argv[]) {
     servaddr.sin_family      = AF_INET;
     servaddr.sin_port        = htons(port);
 
-
-    if ( inet_aton(sAddr, &servaddr.sin_addr) <= 0 ) {
-	printf("ECHOCLNT: Invalid remote IP address.\n");
-	exit(EXIT_FAILURE);
-    }
-
     //remote IP address is stored in the struct 
-    if ( inet_aton(sAddr, &servaddr.sin_addr) <= 0 )
+    if ( inet_aton(sAddr, &servaddr.sin_addr) <= 0 ) 
     {
-        printf("ECHOCLNT: Invalid remote IP address.\n");
-        exit(EXIT_FAILURE);
+    printf("Incorrect remote IP address.\n");
+    exit(EXIT_FAILURE);
     }
- 
-	connectionHandler(conn_s, type, readFile, saveFile);
+
+    if ( connect(conn_s, (struct sockaddr *) &servaddr, sizeof(servaddr) ) < 0 ) 
+    {
+    printf("Could not call connect()\n");
+    exit(EXIT_FAILURE);
+    }
 
     // Read the state from the socket  
+    connectionHandler(conn_s, type, readFile, saveFile);
+
+    // printing message base on the state
     read(conn_s, &state, sizeof(int));
 
-  	// printing message base on the state
-    if (state == 0)
+    if (state == 0) 
     {
-        printf("There was an error in the format \n");
-    } else
+        printf("Format Error\n");
+    } 
+    else 
     {
-        printf("Program executed sucessfully\n");
+        printf("Success\n");
     }
 
-    return EXIT_SUCCESS;}
+    return EXIT_SUCCESS;
+}
 
-
-
-
-
-int ParseCmdLine(int argc, char *argv[], char **sAddr, char **sPort, char **readFile, char** type, char** saveFile) {
-
+int ParseCmdLine(int argc, char *argv[], char **sAddr, char **sPort, char **readFile, char** type, char** saveFile)
+{
   // checking if all the arguments are present
-
     if (argv[1] == NULL) 
     {
-        perror("Server address missing");
+        perror("Must provide server address");
         exit(1);
     }
 
-    if (argv[2] == NULL)
+    if (argv[2] == NULL) 
     {
-        perror("Port number missing");
+        perror("Must provide port number");
         exit(1);
     }
 
     if (argv[3] == NULL) 
     {
-        perror("Reading file name missing");
+        perror("Must provide reading file name");
         exit(1);
     }
 
-    if (argv[4] == NULL)
+    if (argv[4] == NULL) 
     {
-        perror("Host name missing");
+        perror("Must provide host name");
         exit(1);
     }
   
-    if (argv[5]==NULL) 
+    if (argv[5]==NULL)
     {
-        perror("File name of output file missing");
+        perror("Must provide file_name.");
         exit(1);
     }
-   
+
     *sAddr = argv[1];
     *sPort = argv[2];
     *readFile = argv[3];
@@ -156,30 +145,34 @@ int ParseCmdLine(int argc, char *argv[], char **sAddr, char **sPort, char **read
     return 0;
 }
 
+
+
 // reads data from file, creates the header and sends the data to the server
-void connectionHandler(int conn_s, char* type, char* readFile, char* saveFile) {
+void connectionHandler(int conn_s, char* type, char* readFile, char* saveFile) 
+{
 
     unsigned char buffer[1];                        // reading buffer initialized
-
-    int valueArray[LINE_MAXIMUM + HEADER_LINE];      // buffer to store data and header
+    int valueArray[LINE_MAXIMUM+HEADER_LINE];           // buffer to store data and header
     int count = 1;                                  // intial buffer position
     int i;                                          // looping variable
     int type_size = strlen(type);                   // getting length of type
-    int saveFile_size = strlen(saveFile);           // getting length of server file name
+    int saveFile_size = strlen(saveFile);     // getting length of server file name
     
 
-// adding type size and type to the value array    
+ // adding type size and type to the value array       
     valueArray[count] = type_size;
-    count++;                                        // incrementing buffer postion
-    for (i=0; i<type_size; i++) {
+    count++;                                       
+    for (i = 0; i <type_size; i++) 
+    {
         valueArray[count] = type[i];
         count++;
     }
 
     //Copying file_name size and file_name in value array  
     valueArray[count] = saveFile_size;
-    count++;                                         // incrementing buffer postion
-    for (i=0; i<saveFile_size; i++) {
+    count++;                                         
+    for (i=0; i<saveFile_size; i++)
+    {
         valueArray[count] = saveFile[i];
         count++;
     }
@@ -188,41 +181,39 @@ void connectionHandler(int conn_s, char* type, char* readFile, char* saveFile) {
     FILE *ptr;
     ptr=fopen(readFile,"rb");                    
     // checking error while reading the file
-    if (!ptr) { 
+    if (!ptr) 
+    { 
         perror("Unable to open file!"); 
         exit(1);
-    
     }
 
-// checking if the file length exceeds 1000 byets
+    // checking if the file length exceeds 1000 byets
     fseek(ptr, 0, SEEK_END);
     int lengthOfFile = ftell(ptr);
     rewind(ptr);
 
-
-// show error if file length exceeds 1000 bytes    
-    if (lengthOfFile > 1000) {
+    // show error if file length exceeds 1000 bytes    
+    if (lengthOfFile > 1000) 
+    {
         perror("File length exceed!");
         exit(1);
-
     }
 
    //Reading the file 1 byte at a time and saving into valueArray as integer
-
-    while (fread(&buffer,sizeof(buffer),1,ptr) != 0) {
+    while (fread(&buffer,sizeof(buffer),1,ptr) != 0) 
+    {
         /*  Saving the file buffer into data array  */
         valueArray[count] = buffer[0];
         count += 1;
     }
 
-// adding count to the beginning of the buffer
+    // adding count to the beginning of the buffer
     valueArray[0] = count;                      
 
-// writing data as integer to the socket
+    // writing data as integer to the socket
     write(conn_s, &valueArray, sizeof(int)*count);  
     fclose(ptr);                                    
 
 }
-
 
 
